6ad695b0a8e826e6180b382744c4b7717090ade1
mustafa <msolmaz83@gmail.com>
2023-11-14 21:50:48 +0100

    register

---
+++ backend/controllers/user.js
@@ -0,0 +1,42 @@
+const User = require('../models/user.js')
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+
+const register = async(req,res)=>{
+    const {name,email,password} = req.body 
+    const user= await User.findOne({email})
+    if(user) {
+        return res.status(500).json({message:'die Emailhat schon'})
+    }
+    const passwordHash = await bcrypt.hash(password,10)
+    if(password.length < 6){
+        return res.status(500).json({message:'password ist kurty'})
+    }
+    const newUser = await User.create({name,email,password:passwordHash})
+    const token = await jwt.sign({id:newUser._id},process.env.SECRET,{expiresIn:'1h'});
+
+    const cookieOptions = {
+        httpOnly:true,
+        expires:new Date(Date.now( + 5 *24*60*60*1000))
+    }
+    res.status(201).cookie('token',token,cookieOptions).json({
+        newUser,token
+    })
+}
+const login = async(req,res)=>{
+
+}
+const logout = async(req,res)=>{
+
+}
+const forgotPassword = async(req,res)=>{
+
+}
+const resetPassword = async(req,res)=>{
+
+}
+
+
+module.exports = {
+    register,login,forgotPassword,resetPassword,logout
+}
