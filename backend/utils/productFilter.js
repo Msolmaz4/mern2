99a8f925a659342c4bdeb37e2def75b6d5ac0766
mustafa <msolmaz83@gmail.com>
2023-11-14 19:35:31 +0100

    filter

--- backend/utils/productFilter.js
+++ backend/utils/productFilter.js
@@ -15,6 +15,14 @@ class ProductFilter {
 
     }
     filter(){
+        const queryCopy = {...this.queryStr}
+        const deleteArea = ['keyword','page','limit']
+        deleteArea.forEach(item=> delete queryCopy[item])
+        const queryStr = JSON.stringify(queryCopy)
+        queryStr = queryStr.replace(/\b(gt|gte|lt|lte|)\b/g,key=>`$${key}`)
+
+        this.query = this.query.find(JSON.parse(queryStr))
+        return this;
 
     }
     pagination(){
